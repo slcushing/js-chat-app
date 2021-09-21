25b6aac5423f79636f9c2d29cefaebddf79cde70
Stephanie Cushing <stephaniecushing@Stephanies-MacBook-Air.local>
2021-09-21 16:28:51 -0400

    Injected HTML, Event Listener on Submit

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 (function() {
     'use strict';
 
+    const $messageBox = document.querySelector('ul');
+
     function generateHTML(message) {
-        // create some html and inject into the DOM
-        console.log('message', message);
+        let li = document.createElement('li');
+        li.textContent = message.text;   // create some html and inject into the DOM
+        $messageBox.appendChild(li);
     }
 
     /// RETRIEVE MESSAGES FROM API
 
-    fetch('https://tiny-taco-server.herokuapp.com/herdingcats/')
+    fetch('https://tiny-taco-server.herokuapp.com/herdcats/')
     .then(response => response.json())
     .then(messages => {
         messages.forEach(message => generateHTML(message));
@@ -17,36 +20,25 @@
 
     /// CREATE A NEW MESSAGES AND SAVE IT TO THE DATABASE
 
-    const message = {
-        username: 'Stephanie',
-        text: document.querySelector('input').value,
-    }
 
-    fetch('https://tiny-taco-server.herokuapp.com/herdingcats/', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(message),
-        })
-        .then(response => response.json())
-        .then(message => generateHTML(message))
-        .catch(error => console.log('Whoopsie...cat got your tongue?', error))
-    
+    document.querySelector('form').addEventListener('submit', function(event) {
+        event.preventDefault();
 
-   
-    
-    // const message = {
-        
-    // }
+        const message = {
+            username: 'Stephanie',
+            text: document.querySelector('input').value,
+        };
 
-  
-
-
-
-
-
-
-
+        fetch('https://tiny-taco-server.herokuapp.com/herdcats/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(message),
+            })
+            .then(response => response.json())
+            .then(message => generateHTML(message))
+            .catch(error => console.log('Whoopsie...cat got your tongue?', error));        
+    });
 
 })();
