abbf739a6bef642ae45efb47b0a472847c7e5857
Stephanie Cushing <stephaniecushing@Stephanies-MacBook-Air.local>
2021-09-21 13:39:42 -0400

    Random Push

--- main.js
+++ main.js
@@ -1,1 +1,39 @@
-console.log('hey vinny')
+(function(){
+    'use strict';
+
+    fetch('https://tiny-taco-server.herokuapp.com/herdingcats/')
+    .then(response => response.json())
+    .then(data => console.log(data))
+
+
+    // const username = {
+    //     text: 'Stephanie',
+    // }
+    
+    // fetch('https://tiny-taco-server.herokuapp.com/herdingcats/', {
+    //     method:'POST',
+    //     body: JSON.stringify(username),
+    // })
+    // .then(response => response.json())
+    // .then(data => console.log(data))
+    // .catch(error => console.log('Error', error))
+        
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
